df3f462e2a118146b52d2f0021f9cb304eafcf97
Selim <ahmetselimden@gmail.com>
2026-03-27 20:14:19 +0300

    fix: html2pdf import duzeltildi

--- src/pages/AIPlanUretici.jsx
+++ src/pages/AIPlanUretici.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import { useState } from "react";
-import html2pdf from 'html2pdf.js';
+import html2pdf from "html2pdf.js";
 const styles = `
   @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap');
   *, *::before, *::after { box-sizing: border-box; margin: 0; padding: 0; }
